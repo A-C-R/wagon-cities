1019ab68675e7651cee530cec3d460f4d6c22810
Anne Rohde <rohde.anch@gmail.com>
2020-01-15 21:55:39 +0100

    t

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,11 @@
 // TODO: add and export your own actions
+import { combineReducers } from 'redux';
+import CitiesReducer from '../reducers/cities_reducer';
+import ActiveCityReducer from '../reducers/active_city_reducer';
+
+const rootReducer = combineReducers({
+  cities: CitiesReducer,
+  activeCity: ActiveCityReducer
+});
+
+export default rootReducer;
